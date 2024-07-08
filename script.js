f839950f364ad05c21358312e6c31a3057571aca
Nikolay Pashov <nikolaypashov9@gmail.com>
2024-07-08 18:46:53 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,78 @@
+const billInput = document.getElementById("bill-size");
+const tips = document.querySelectorAll(".btn")
+const customTip = document.getElementById("custom");
+const peopleInput = document.getElementById("people-count");
+const tipPerPerson = document.getElementById("tip-person");
+const totalPerson = document.getElementById("total-person");
+const resetBtn = document.getElementById("reset-btn");
+const zero = document.getElementById('zero');
+
+
+billInput.addEventListener("input", billInputFunc);
+peopleInput.addEventListener("input", peopleInputFunc);
+tips.forEach(function (val) {
+    val.addEventListener('click', handleClick);
+});
+resetBtn.addEventListener('click', reset);
+customTip.addEventListener('input', customInput);
+
+let billValue = 0.0;
+let peopleValue = 1;
+let tipValue = 0.15;
+
+function billInputFunc() {
+    billValue = parseFloat(billInput.value);
+    calculateTip();
+}
+
+function peopleInputFunc() {
+    peopleValue = parseFloat(peopleInput.value)
+    if (peopleInput.value <= 0 ) {
+        zero.style.display = 'block';
+        peopleInput.style.border = "solid 2px #E17457";
+    } else {
+        zero.style.display = 'none';
+    }
+    calculateTip();
+};
+
+function handleClick(event) {
+    tips.forEach(function (btn) {
+        btn.classList.remove("active-tip");
+        if (event.target.innerHTML === btn.innerHTML) {
+            btn.classList.add("active-tip");
+            tipValue = parseFloat(btn.innerHTML) / 100;
+        }
+    })
+    customTip.value = '';
+    calculateTip();
+}
+
+function customInput() {
+    if (customTip.value > 0 ) {
+        tipValue = parseFloat(customTip.value) / 100;
+    }
+    calculateTip();
+}
+
+
+function calculateTip() {
+    if (peopleValue >= 1) {
+        let tipAmount = (billValue * tipValue) / peopleValue;
+        let total = (billValue / peopleValue) + tipAmount;
+        tipPerPerson.innerHTML = "$" + tipAmount.toFixed(2);
+        totalPerson.innerHTML = "$" + total.toFixed(2);
+    } 
+};
+
+
+
+function reset() {
+    billInput.value = '';
+    peopleInput.value = '';
+    customTip.value = '';
+    tipPerPerson.innerHTML = '$0.00';
+    totalPerson.innerHTML = '$0.00';
+    peopleInput.style.border = 'hidden'
+    zero.style.display = 'none'
+}
